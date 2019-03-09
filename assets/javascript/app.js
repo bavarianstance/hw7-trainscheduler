7712e96401be252711b30ae4c4d9a6755e2f90c3
bavarianstance <bavarianstance@gmail.com>
2019-03-08 18:11:05 -0800

    added comments in html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+ // Firebase Config 
+
  let config = {
     apiKey: "AIzaSyDzIYjALeTyduRWvXxtVd8W0czr-KCwmhQ",
     authDomain: "hk-highspeed-rail.firebaseapp.com",
@@ -6,17 +8,18 @@
     storageBucket: "",
     messagingSenderId: "623260624993"
   };
-
+// Firebase Initialization
   firebase.initializeApp(config);
 
   let database = firebase.database();
 
+// current time function
   let currentTime = () => {
   	let current = moment().format('LT');
   	$("#currentTime").html(current);
   	setTimeout(currentTime,1000);
   };
-
+// add train onclick listener and function
   $("#btn-add-train").on("click", (event) => {
   	event.preventDefault();
 
@@ -24,16 +27,16 @@
   	let trainDestination = $("#destination-name").val().trim();
   	let firstTrainTime = $("#first-train-time").val().trim();
   	let trainFrequency = $("#frequency").val().trim();
-
+// firebase data attribution
   	let newTrain = {
   		name: trainName,
   		destination: trainDestination,
   		firstTime: firstTrainTime,
   		frequency: trainFrequency
   	};
-
+// push new train input values to firebase
   database.ref().push(newTrain);
-
+// reset form input values
   $("#train-name").val("");
   $("#destination-name").val("");
   $("#first-train-time").val("");
@@ -41,13 +44,14 @@
 
   });
 
+// append html dynamically using firebase
 database.ref().on("child_added", (childSnapshot) => {
 
 	let trainName = childSnapshot.val().name;
 	let trainDestination = childSnapshot.val().destination;
 	let firstTrainTime = childSnapshot.val().firstTime;
 	let trainFrequency = childSnapshot.val().frequency;
-
+// calculations for train timing / schedule
 	let firstTrainTimeCalc = moment(firstTrainTime, "hh:mm a").subtract(1, "years");
 	let time = moment().format("HH:mm a");
 	let timeDifference = moment().diff(moment(firstTrainTimeCalc), "minutes");
@@ -57,8 +61,9 @@ database.ref().on("child_added", (childSnapshot) => {
 
  	$("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + trainArrival + "</td><td>" + nextTrain + "</td></tr>");
 },
+// handling errors
 (errorObject) => {
 //console.log("Errors : " + errorObject.code)
 });
-
+//run current time function
 currentTime();
